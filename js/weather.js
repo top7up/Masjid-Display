7b052dd9ef2336b39a643302d8f37c46679b8d76
AbdulAziz A AlOraij <32993096+top7up@users.noreply.github.com>
2019-09-06 22:59:23 +0300

    Create weather.js

---
+++ js/weather.js
@@ -0,0 +1,32 @@
+const tempElement = document.getElementById('temperature');
+const iconElement = document.getElementById('icon');
+const weatherStatsElement = document.getElementById('weather-stats');
+
+function weather(city, tempType, apiKey) {
+  let currentIcon = '';
+  let iconURL = 'https://openweathermap.org/img/w/';
+  fetch(`http://api.openweathermap.org/data/2.5/weather?q=${city}&units=imperial&appid=${apiKey}`)
+    .then(res => res.json())
+    .then(data => {
+      const { main, weather, wind } = data;
+      const temp = convertTemp(tempType, main.temp);
+	  //temp = int(temp);
+	  var inttemp = Math.round( temp );
+      tempElement.innerHTML = `الرياض - ${inttemp}&deg;`;
+      
+      iconURL += `${weather[0].icon}.png`;
+      iconElement.setAttribute('src', iconURL);
+      iconElement.innerHTML = weather[0].main;
+      weatherStatsElement.innerHTML = `سرعة الرياح: ${wind.speed}\n<br>نسبة الرطوبة: ${main.humidity}%`;
+    })
+    .catch(err => console.log(err));
+}
+
+function convertTemp(tempType, fh) {
+  if (tempType.toUpperCase() === 'C') {
+    // (32°F − 32) × 5/9 = 0°C
+    return (fh - 32) * 5 / 9;
+  } else {
+    return fh
+  }
+}
